3434cc7f632d63aa5e701abc2a3a2fd0f126e1c8
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-10-26 11:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let elem = document.getElementById('colorSelect');
+let btn = document.querySelector('input');
+let selectedOption = '';
+
+// console.log(btn.previousElementSibling);
+// console.log(elem[0]);
+btn.addEventListener('click',() => {
+    elem.removeChild(selectedOption);
+})
+
+
+
+elem.addEventListener('click', function() {
+  const selectedIndex = this.selectedIndex; // Get the index of the selected option
+  selectedOption = this.options[selectedIndex]; // Get the selected option element
+});
